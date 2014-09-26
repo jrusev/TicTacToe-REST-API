efe03be96baa74d7961c6a137480707411fd86ae
neutrino <jrusev@gmail.com>
2014-09-26 23:24:49 +0300

    added sign forms

--- public/js/controllers/MainPageController.js
+++ public/js/controllers/MainPageController.js
@@ -4,6 +4,7 @@ ticTacToeApp.controller('MainPageController',
     function MainPageController($scope, $rootScope, author, copyright, auth) {
         $scope.author = author;
         $scope.copyright = copyright;
+        //$scope.isSearchInputShown = true;
 
         if (auth.isAuthenticated()) {
             $rootScope.isLoggedIn = true;
