531b0278949310daa22140c3377eb8f8d22a4806
neutrino <jrusev@gmail.com>
2014-09-27 16:14:48 +0300

    added search

--- public/js/controllers/ListUsersController.js
+++ public/js/controllers/ListUsersController.js
@@ -2,6 +2,7 @@
 
 ticTacToeApp.controller('ListUsersController',
     function ListUsersController($scope, ticTacToeData) {
+        $scope.sort = 'Id';
         ticTacToeData
             .getUsers()
             .then(function (data) {
